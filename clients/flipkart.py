c6b5f092a36d9eed4db80cce05e5f17b9c051f96
Vivek Anand <vivekanand1101@gmail.com>
2016-04-21 01:41:43 +0530

    add missing import

--- clients/flipkart.py
+++ clients/flipkart.py
@@ -4,7 +4,7 @@ import json
 payload = {
     'product_id': 'xyz',
     'website': 'flipkart.com',
-    'url': 'http://www.flipkart.com/samsung-galaxy-j7/p/itmeafbfjhsydbpw?pid=MOBE93GWSMGZHFSK',
+    'url': 'http://www.flipkart.com/smart-dealz-ubon-ub-1085-designer-gaming-canal-earphone-microphone-iphone-samsung-htc-mi-super-bass-headphone-headphones/p/itmeexk4qfdqgugr?pid=ACCEEXK43UGW5WPC&al=Lk30qATLzjGzbnI%2FLIQHesldugMWZuE7sHPMhtl4IOrzzpSVAIs5cIbfXqmuuGt4GJ81eFD8hSY%3D&ref=L%3A2153276897080594144&srno=b_3',
     'email': 'vivekanand1101@gmail.com',
 }
 
