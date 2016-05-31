9b8573fd0729c8a8eb4b197eb772ec007cd34b0b
Saptak Sengupta <saptak013@gmail.com>
2016-05-31 10:36:11 +0530

    build.sh added

---
+++ build.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python-pip libxml2-dev libxslt1-dev python-dev zlib1g-dev libffi-dev libssl-dev libevent-dev postgresql postgresql-contrib python-numpy python-scipy python-sklearn libpq-dev
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
